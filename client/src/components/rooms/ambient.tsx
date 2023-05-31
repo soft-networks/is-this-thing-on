8aa6807a921c07ed9497ed52bbda195893ca3664
Chia Amisola <chiamisola@gmail.com>
2023-05-31 14:23:08 -0700

    ambienteintent

--- client/src/components/rooms/ambient.tsx
+++ client/src/components/rooms/ambient.tsx
@@ -5,7 +5,7 @@ import Stickers from "../stickers";
 
 const Ambient: React.FC = () => {
   return (
-    <div className="fullBleed overflowScroll">
+    <div className="fullBleed overflowScroll" style={{background: "#edf3f4"}}>
       <VideoPlayer
         style={{
           width: "100%",
@@ -13,24 +13,27 @@ const Ambient: React.FC = () => {
           zIndex: 0
         }}
         className="noEvents absoluteOrigin"
-  
       />
       
-    <iframe
-      src="https://ambient.institute/sounds/" 
-      width="100%" height="100%" />
+    <iframe className="ambience" 
+      src="https://ambient.institute/sounds"  />
       
       <Chat
         className="quarterWidth absoluteOrigin"
         style={
           {
-            "--chatAuthorColor": "hotpink",
-            "--chatContainerBackground": "rgba(0,0,0,0.6)",
+            "--chatAuthorColor": "#d3ecef",
+            "--chatContainerBackground": "rgba(0,0,0,0.1)",
             "--chatMessageColor": "var(--white)",
             zIndex: 3,
           } as React.CSSProperties
         }
       />
+      
+      <Stickers className="noEvents" style={{
+          width: "100%",
+          height: "100%"
+        }}/>
     </div>
   );
 }
