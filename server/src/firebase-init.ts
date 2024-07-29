975bad32c0a420aa16b1089cb24969e186f109ef
bhavik singh <bhavik@bhaviksworkbook.lan>
2024-07-28 22:58:11 -0400

    removing api key from github not needed anymore

--- server/src/firebase-init.ts
+++ server/src/firebase-init.ts
@@ -14,12 +14,4 @@ export const app = initializeApp({
   databaseURL: "https://is-this-thing-on-320a7-default-rtdb.firebaseio.com",
   storageBucket: "is-this-thing-on-320a7.appspot.com"
 });
-export const firebaseConfig = {
-  apiKey: "AIzaSyDEELIQs6LfHdFCnqUUNluk7tXKodeHIwE",
-  authDomain: "is-this-thing-on-320a7.firebaseapp.com",
-  databaseURL: "https://is-this-thing-on-320a7-default-rtdb.firebaseio.com",
-  projectId: "is-this-thing-on-320a7",
-  messagingSenderId: "895037288643",
-  appId: "1:895037288643:web:77cdcfd1981d449fc6b276"
-};
 export const firestore = getFirestore(app);
