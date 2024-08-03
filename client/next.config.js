d79fa5e0ed6a050011d6cb2a88a47102b480d956
Kevin Yeh <kevinyeah@utexas.edu>
2024-08-03 17:12:55 -0400

    Revert "wip"
    This reverts commit 9561cce84ab60d9552533c79682d833113dbe249.

--- client/next.config.js
+++ client/next.config.js
@@ -1,25 +1,6 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config, { dev, isServer }) {
-    if (dev && !isServer) {
-      const originalEntry = config.entry;
-      // eslint-disable-next-line no-param-reassign
-      config.entry = async () => {
-        const wdrPath = path.resolve(__dirname, "./src//wdyr.ts");
-        const entries = await originalEntry();
-
-        if (entries["main.js"] && !entries["main.js"].includes(wdrPath)) {
-          entries["main.js"].push(wdrPath);
-        }
-        return entries;
-      };
-    }
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
