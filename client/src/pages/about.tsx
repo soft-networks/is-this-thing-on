111969ed32425da10e6a9f5521fff542e1020bc9
ROTHBERG <sarahrothberg@gmail.com>
2024-09-09 14:06:15 -0400

    Update about.tsx
    fixed some spacing issues

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -19,10 +19,7 @@ const About: NextPage = () => {
       </Head>
       <div className="center:absolute highestLayer">
       <a href="/">thing.tube</a> is an experimental live-streaming platform for artists, founded by the collective “is this thing on?” 
-      (<a href="https://christopherclary.com/">Christopher Clary</a>, 
-      <a href="https://sarahrothberg.com/"> Sarah Rothberg</a>, 
-      <a href="https://mollysoda.exposed/"> Molly Soda</a>,
-      and <a href="https://softnet.works/">Bhavik Singh</a>). 
+      (<a href="https://christopherclary.com/">Christopher Clary</a>, <a href="https://sarahrothberg.com/">Sarah Rothberg</a>, <a href="https://mollysoda.exposed/">Molly Soda</a>, and <a href="https://softnet.works/">Bhavik Singh</a>). 
       <br/>
       <br/>
       The platform is developed as a process of <i>software creation as performance art</i>. 
@@ -31,16 +28,12 @@ const About: NextPage = () => {
       <br/>
       <br/>
       First launched in 2022, the website has featured performances by 
-      <a href="https://chia.design/">Chia Amisola</a>, 
-      <a href="https://herdimasanggara.com/">Herdimas Anggara</a>, 
-      <a href="https://darlasmessfactory.com">Darla Devour</a>, 
-      <a href="https://mayaontheinter.net/">Maya Man</a>, 
+      <a href="https://chia.design/">Chia Amisola</a>, <a href="https://herdimasanggara.com/">Herdimas Anggara</a>, <a href="https://darlasmessfactory.com">Darla Devour</a>, <a href="https://mayaontheinter.net/">Maya Man</a>,  
       and the founding members. 
       THING is made possible with support from the Gray Area C/Change grant, NEW INC Next Web grant, Onassis ONX, Museum of the Moving Image (MoMI), 
-      and developers 
-      <a href="https://www.markhramos.net">Mark Ramos</a>, 
-      and 
-      <a href="https://kyeh.me/">Kevin Yeh</a>, 
+      and developers <a href="https://www.markhramos.net">Mark Ramos</a>, and <a href="https://kyeh.me/">Kevin Yeh</a>. 
+      <br/>
+      <br/>
       Our code is opensource and archived on our <a href = "https://github.com/soft-networks/is-this-thing-on">github</a>.
       For the full history of our project, feel free to explore our public archive on <a href = "https://drive.google.com/embeddedfolderview?id=1kQnnvjLfiySUOoV8foHPa2sKUNVrGeZm#grid">google drive.</a> 
           <br/>
