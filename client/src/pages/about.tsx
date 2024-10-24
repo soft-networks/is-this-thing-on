61bccf016a85b94476e2edb5b473d40cb3cd03dc
ROTHBERG <sarahrothberg@gmail.com>
2024-10-24 14:00:45 -0400

    updates for NOTHING

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -25,34 +25,42 @@ const About: NextPage = () => {
        <title>about THING</title>
      </Head>
      <div className="center:absolute highestLayer">
-     <Link href="/">
+  
+     <b>
+          ABOUT:
+        </b>
+        <br/>
+        <br/>
+
+     <Link href="/"className="underline">
     thing.tube
      </Link> is an experimental livestreaming platform for artists, founded by the collective “is this thing on?”
-     (<a href="https://christopherclary.com/">Christopher Clary</a>, <a href="https://sarahrothberg.com/">Sarah Rothberg</a>, <a href="https://mollysoda.exposed/">Molly Soda</a>, and <a href="https://softnet.works/">Bhavik Singh</a>).
+     (<a href="https://christopherclary.com/"className="underline">Christopher Clary</a>, <a href="https://sarahrothberg.com/"className="underline">Sarah Rothberg</a>, <a href="https://mollysoda.exposed/"className="underline">Molly Soda</a>, and <a href="https://softnet.works/"className="underline">Bhavik Singh</a>).
      <br/>
      <br/>
-     The platform is developed as a process of <i>software creation as performance art</i>.
-     Its features and form are shaped by our own desires, conceptual questions about online platforms, and the current context of livestreaming and the web.
-     Currently, thing.tube allows performers to stream from customized rooms, connected by a webring and a universal chat.
+     thing.tube is both a performance platform and a form of <i>software development as performance art</i>: we develop it to fit our own creative desires and with conceptual questions in mind about online platforms and the current context of livestreaming and the web. It is always changing.
+     Right now on thing.tube streamers make their own customizable rooms, which are connected by a webring and a universal chat. We typically do group stream performances and announce them here and on our <a href="https://www.instagram.com/thing.tube/" className="underline">IG</a>. 
      <br/>
      <br/>
-     First launched in 2022, the website has featured performances by <a href="https://chia.design/">Chia Amisola</a>, <a href="https://ritual.engineer/">Herdimas Anggara</a>, <a href="https://linktr.ee/darladevoursyou">Darla Devour</a>, <a href="https://mayaontheinter.net/">Maya Man</a>, 
+     First launched in 2022, the website has featured performances by <a href="https://chia.design/" className="underline" >Chia Amisola</a>, <a href="https://ritual.engineer/"className="underline">Herdimas Anggara</a>, <a href="https://linktr.ee/darladevoursyou"className="underline">Darla Devour</a>, <a href="https://mayaontheinter.net/" className="underline">Maya Man</a>, 
      and the founding members.
      THING is made possible with support from the Gray Area C/Change grant, NEW INC Next Web grant, Onassis ONX, Museum of the Moving Image (MoMI),
-     and developers <a href="https://www.markhramos.net">Mark Ramos</a>, and <a href="https://kyeh.me/">Kevin Yeh</a>.
+     and developers <a href="https://www.markhramos.net" className="underline" >Mark Ramos</a>, and <a href="https://kyeh.me/" className="underline">Kevin Yeh</a>.
      <br/>
      <br/>
-     Our code is open source and archived on our <a href = "https://github.com/soft-networks/is-this-thing-on">github</a>.
-     For the full history of our project, feel free to explore our public archive on <a href = "https://drive.google.com/embeddedfolderview?id=1kQnnvjLfiySUOoV8foHPa2sKUNVrGeZm#grid">google drive.</a>
+     Our code is open source and archived on our <a href = "https://github.com/soft-networks/is-this-thing-on" className="underline" >github</a>.
+     For the full history of our project, feel free to explore our public archive on <a href = "https://drive.google.com/embeddedfolderview?id=1kQnnvjLfiySUOoV8foHPa2sKUNVrGeZm#grid" className="underline" >google drive.</a>
          <br/>
          <br/>
-         <s>
-        next stream: 11/09/24 @ MoMI
-        </s>
         <br/>
-        Due to FORCE MAJEURE we have postponed the October 12 MoMI show! In the meantime, we will do NO-THING, a 24-hour stream on the theme of NOTHING, early December. <Link href="https://docs.google.com/forms/d/e/1FAIpQLSdfDAp4BTtW1RZKcfJX3xArCa3cVEJTpMQgd7fz0M5c5_yLlg/viewform?usp=sf_link" className="underline">Let us know if you&apos;d like to participate?
-    </Link>        We don&apos;t have all the details yet, but we&apos;ll get back to you sometime in November.
+        <b>
+          UP NEXT:
+        </b>
         <br/>
+        <br/>
+
+        Due to FORCE MAJEURE our IRL show at <a href = "https://movingimage.org/" className="underline" >MoMI</a> is postponed until Spring 2025! In the meantime, we will do NO-THING, a 24-hour stream on the theme of NOTHING, December 29. <Link href="https://docs.google.com/forms/d/e/1FAIpQLSdfDAp4BTtW1RZKcfJX3xArCa3cVEJTpMQgd7fz0M5c5_yLlg/viewform?usp=sf_link" className="underline">Let us know if you&apos;d like to participate?
+    </Link>      
          </div>
   
 
