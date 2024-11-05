454f498ac9622710b765b3a16df8b943631226fc
Kevin Yeh <kevinyeah@utexas.edu>
2024-11-05 18:26:58 -0500

    add info

--- client/src/components/streamPlayer.tsx
+++ client/src/components/streamPlayer.tsx
@@ -27,6 +27,13 @@ const StreamPlayer: React.FunctionComponent<VideoPlayerProps> = ({
 
   const participants = useParticipants();
 
+  // Enforce syncing on the call object to ensure the stream liveness is updated.
+  // Without this, there are two major issues:
+  //
+  // 1. If a viewer joins the room before the stream is live, their page will not be automatically updated with video/audio when the stream goes live.
+  // 2. If a viewer is in the room when the stream goes from live --> stopped, BUT there's an issue with updating the room status in Firestore,
+  //    they will continue to see the streamer's camera and audio even though the Stream is in "backstage mode".
+  //
   useEffect(() => {
     call?.get();
   }, [call]);
