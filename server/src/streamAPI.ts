d430bed0581a1e5c28494abbb027b9b0ec66c888
Kevin Yeh <kevinyeah@utexas.edu>
2024-11-05 19:27:26 -0500

    revert changes

--- server/src/streamAPI.ts
+++ server/src/streamAPI.ts
@@ -65,16 +65,9 @@ export const streamUpdateWasReceived: RequestHandler = async (req, res) => {
             const id = hook.call_cid;
             const callID = id.split(':')[1];
             logInfo(`Received ${eventType} for call ${callID}`);
-            const call = await client.video.call('livestream', callID).get();
-            
-            if (call.members.length <= 0) {
-                logUpdate("> Stream went idle");
-                let roomID = await getRoomIDFromStreamCallID(callID);
-                await writeStreamStateToDB(roomID, "idle");
-            } else {
-                logInfo("Call still has members online.")
-            }
-            
+            logUpdate("> Stream went idle");
+            let roomID = await getRoomIDFromStreamCallID(callID);
+            await writeStreamStateToDB(roomID, "idle");                
             return res.status(200).send("Thanks for the update :) ");    
         }
         if (eventType == "custom" && hook.custom.type == "STOP_LIVE") {
