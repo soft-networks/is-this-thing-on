6c25db82f794d9139aa69830508f0c91274312a6
Kevin Yeh <kevinyeah@utexas.edu>
2024-12-21 18:46:31 -0500

    revert memoization

--- client/src/components/video/videoPreview.tsx
+++ client/src/components/video/videoPreview.tsx
@@ -1,18 +1,29 @@
-import ReactPlayer from "react-player";
+import { useEffect, useMemo } from "react";
 
-const TEST_VIDEO =
-  "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8";
+import ReactPlayer from "react-player";
+import StreamPlayer from "./streamPlayer";
+import { generateStreamLink } from "../../lib/server-api";
+import { logVideo } from "../../lib/logger";
 
 const VideoPreview: React.FC<{
   iLink: RoomSummary;
   localMuted: boolean;
   isTest: boolean;
 }> = ({ iLink, localMuted, isTest }) => {
-  return !isTest && !iLink.streamHlsPlaylistID ? (
-    <></>
-  ) : (
+  const streamLink = useMemo(() => {
+    if (isTest) {
+      return "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8";
+    }
+    return iLink.streamHlsPlaylistID;
+  }, [iLink.streamHlsPlaylistID, isTest]);
+
+  useEffect(() => {
+    if (streamLink) logVideo(iLink.roomName, streamLink);
+  }, [iLink.roomName, streamLink]);
+
+  return (
     <ReactPlayer
-      url={isTest ? TEST_VIDEO : iLink.streamHlsPlaylistID}
+      url={streamLink}
       playing={true}
       muted={localMuted}
       className="noEvents "
