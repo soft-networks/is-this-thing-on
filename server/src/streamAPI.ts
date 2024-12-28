a33b77c158d94b4c36baaf9a6154cf0b3562e298
Bhavik Singh <bhaviksingh@gmail.com>
2024-12-28 14:19:41 -0500

    server hotfix (#62)
    Quick fix to replace us-east url's as well as ohio ones in server.
    Already pushed to prod and tested in prod.
    
    ---------
    
    Co-authored-by: bhavik singh <bhavik@bhaviks-WorkBook.local>

--- server/src/streamAPI.ts
+++ server/src/streamAPI.ts
@@ -106,7 +106,9 @@ export const streamUpdateWasReceived: RequestHandler = async (req, res) => {
       console.log("recording ready for roomID: ", hook);
       const recording = hook.call_recording;
       if (recording) {
-        const ref = await writeRecordingToDB(roomID, recording.url.replace("ohio.stream-io-cdn.com/", "storage.googleapis.com/is-this-thing-on-recordings//"), recording.start_time, recording.end_time);
+        
+        const newRecordingURL = recording.url.replace("ohio.stream-io-cdn.com/", "storage.googleapis.com/is-this-thing-on-recordings//");
+        const ref = await writeRecordingToDB(roomID, newRecordingURL, recording.start_time, recording.end_time);
         logUpdate(`Wrote recording to DB for room ${roomID} with ref ${ref.id}`);
       } else {
         logError(`Recording was ready but didn't exist`);
