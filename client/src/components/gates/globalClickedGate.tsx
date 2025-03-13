3f4626ece8572c94a9afe7b8a49d2b5e6d180e6d
bhavik singh <bhavik@bhaviks-WorkBook.local>
2025-03-12 20:50:33 -0400

    copyupdate

--- client/src/components/gates/globalClickedGate.tsx
+++ client/src/components/gates/globalClickedGate.tsx
@@ -20,13 +20,12 @@ const ClickGate: React.FunctionComponent = ({ children }) => {
         className="center:absolute uiLayer center-text padded narrow stack:s2 cursor:pointer"
       >
         <div className="h1">
-          WELCOME TO NO-THING<br />
+          WELCOME TO THING<br />
         </div>
         <div className="h3 stack">
-          <p>NO-THING was on Dec 29 for 24hours, with 20+ streamers streaming together on the theme of nothing.</p>
-          <p>It is now over. Enjoy looping archives of the streams.</p>
+          <p>Our next performance, THING+YOU is coming to the Museum of the Moving Image (MoMI) April 12th.</p>
+          <p>In the meantime enjoy an archive of NO-THING, a 24-hour performance that took place this past Dec 29th with more than 30+ streamers.</p>
         </div>
-
         <div className="h1">
           <em>CLICK 2 ENTER</em>
         </div>
